b7dcf42167f7c64f7238b79f0f4dfc7a285900f2
marcelo contreras <intohybrid@gmail.com>
2022-01-24 00:30:27 -0300

    test validar ramas

--- vars/integracioncontinua.groovy
+++ vars/integracioncontinua.groovy
@@ -1,11 +1,11 @@
 def call(Map pipelineParameters){
     if ((env.BRANCH_NAME =~ '.*feature.*').matches()) {
         echo "llegamos a featTUre"
-        ci
+        ci pipelineParameters
         echo "terminamos aca"
     }
 }
-def ci(){
+def ci(Map pipelineParameters){
         
     pipeline {
         agent any
