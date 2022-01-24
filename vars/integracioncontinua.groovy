c0404b004338c3199fd17cbb863023c838460d26
marcelo contreras <intohybrid@gmail.com>
2022-01-23 23:08:46 -0300

    probando si se imprime variables de entorno

--- vars/integracioncontinua.groovy
+++ vars/integracioncontinua.groovy
@@ -12,8 +12,6 @@ def ci(){
                 sh "echo 'Compile Code!'"
                 // Run Maven on a Unix agent.
                 //sh "mvn clean compile -e"
-                }
-
             }
             stage("2 Unit Test"){
             //- Testear el código con comando maven
@@ -33,7 +31,6 @@ def ci(){
             //- Cada ejecución debe tener el siguiente formato de nombre: QUE ES EL NOMBRE DE EJECUCIÓN ??
                 //- {nombreRepo}-{rama}-{numeroEjecucion} ejemplo:
                 //- ms-iclab-feature-estadomundial(Si está usando el CRUD ms-iclab-feature-[nombre de su crud])
-
                 withSonarQubeEnv('SonarQubeServer') {
                     sh "echo 'SonarQube'"
                     sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=githubfull'
@@ -53,8 +50,6 @@ def ci(){
                                     groupId: 'com.devopsusach2020', 
                                     packaging: 'jar', 
                                     version: VERSION]]]
-
-                }
             }
             stage("6 gitCreateRelease"){
             //- Crear rama release cuando todos los stages anteriores estén correctamente ejecutados.
