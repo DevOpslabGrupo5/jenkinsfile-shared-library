3253b74e42538a68b515d7d018e27b8937305804
Marcelo Contreras <intohybrid@gmail.com>
2022-01-28 00:04:39 -0300

    slack

--- vars/integracioncontinua.groovy
+++ vars/integracioncontinua.groovy
@@ -51,37 +51,37 @@ def call(Map pipelineParameters){
                 steps {
                     sh "echo 'Build .Jar!'"
                     // Run Maven on a Unix agent.
-                    sh "mvn clean package -e"
+                    //sh "mvn clean package -e"
                 }
             }
-            stage("4 SonarQube"){
-            //- Generar análisis con sonar para cada ejecución
-            //- Cada ejecución debe tener el siguiente formato de nombre: QUE ES EL NOMBRE DE EJECUCIÓN ??
-                //- {nombreRepo}-{rama}-{numeroEjecucion} ejemplo:
-                //- ms-iclab-feature-estadomundial(Si está usando el CRUD ms-iclab-feature-[nombre de su crud])
-                steps {
-                    withSonarQubeEnv('SonarQubeServer') {
-                        sh "echo 'SonarQube'"
-                        sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=githubfull'
-                    }
-                }
-                // post {
-                //     //- Subir el artefacto creado al repositorio privado de Nexus.
-                //     //- Ejecutar este paso solo si los pasos anteriores se ejecutan de manera correcta.
-                //     success {
-                //         nexusPublisher nexusInstanceId: 'nexus', 
-                //         nexusRepositoryId: 'devops-usach-nexus', 
-                //         packages: [[$class: 'MavenPackage', 
-                //             mavenAssetList: [[classifier: '', 
-                //                             extension: '',
-                //                             filePath: 'build/DevOpsUsach2020-0.0.1.jar']],
-                //             mavenCoordinate: [artifactId: 'DevOpsUsach2020', 
-                //                             groupId: 'com.devopsusach2020', 
-                //                             packaging: 'jar', 
-                //                             version: VERSION]]]
-                //     }
-                // }
-            }
+            // stage("4 SonarQube"){
+            // //- Generar análisis con sonar para cada ejecución
+            // //- Cada ejecución debe tener el siguiente formato de nombre: QUE ES EL NOMBRE DE EJECUCIÓN ??
+            //     //- {nombreRepo}-{rama}-{numeroEjecucion} ejemplo:
+            //     //- ms-iclab-feature-estadomundial(Si está usando el CRUD ms-iclab-feature-[nombre de su crud])
+            //     steps {
+            //         withSonarQubeEnv('SonarQubeServer') {
+            //             sh "echo 'SonarQube'"
+            //             sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=githubfull'
+            //         }
+            //     }
+            //     post {
+            //         //- Subir el artefacto creado al repositorio privado de Nexus.
+            //         //- Ejecutar este paso solo si los pasos anteriores se ejecutan de manera correcta.
+            //         success {
+            //             nexusPublisher nexusInstanceId: 'nexus', 
+            //             nexusRepositoryId: 'devops-usach-nexus', 
+            //             packages: [[$class: 'MavenPackage', 
+            //                 mavenAssetList: [[classifier: '', 
+            //                                 extension: '',
+            //                                 filePath: 'build/DevOpsUsach2020-0.0.1.jar']],
+            //                 mavenCoordinate: [artifactId: 'DevOpsUsach2020', 
+            //                                 groupId: 'com.devopsusach2020', 
+            //                                 packaging: 'jar', 
+            //                                 version: VERSION]]]
+            //         }
+            //     }
+            // }
             stage("6 gitCreateRelease"){
             //- Crear rama release cuando todos los stages anteriores estén correctamente ejecutados.
             //- Este stage sólo debe estar disponible para la rama develop.
